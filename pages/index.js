4dfe3cb79e8fc8ad5b57e0ffc5e571ae34f00965
NewWorldOrderly <gobuffs10@gmail.com>
2022-05-28 21:25:13 -0700

    chore: prettier

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,89 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="padding-none bg-cover bg-animal-collective bg-no-repeat bg-fixed">
+    <div className="padding-none bg-animal-collective bg-cover bg-fixed bg-no-repeat">
       <Head>
         <title>Bryan Duckworth - Perception is an illusion.</title>
-        <meta name="description" content="Versatile problem solver with over 15 years of web development experience." />
+        <meta
+          name="description"
+          content="Versatile problem solver with over 15 years of web development experience."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="py-16 flex flex-1 flex-col justify-center align-center min-h-screen	">
-        <h1 className="m-0 text-6xl text-surface text-shadow-md text-center">
+      <main className="align-center flex min-h-screen flex-1 flex-col justify-center py-16	">
+        <h1 className="text-shadow-md m-0 text-center text-6xl text-surface">
           Perception is an illusion.
         </h1>
 
-        <p className="text-center text-shadow text-surface text-2xl my-16">
-          <code className="bg-surface text-onSurface text-shadow-none p-2 text-xl inline-block mb-4">NextJS</code><br className="md:hidden"/>
-          <span className="inline-block mb-4 md:mx-4">{` </> `}</span><br className="md:hidden"/>
-          <code className="bg-surface text-onSurface text-shadow-none p-2 text-xl inline-block mb-4">React</code><br className="md:hidden"/>
-          <span className="inline-block mb-4 md:mx-4">{` </> `}</span><br className="md:hidden"/>
-          <code className="bg-surface text-onSurface text-shadow-none p-2 text-xl inline-block mb-4">Node.js</code><br className="md:hidden"/>
-          <span className="inline-block mb-4 md:mx-4">{` </> `}</span><br className="md:hidden"/>
-          <code className="bg-surface text-onSurface text-shadow-none p-2 text-xl inline-block mb-4">TailwindCSS</code><br className="md:hidden"/>
-          <span className="inline-block mb-4 md:mx-4">{` </> `}</span><br className="md:hidden"/>
-          <code className="bg-surface text-onSurface text-shadow-none p-2 text-xl">Git</code>
+        <p className="text-shadow my-16 text-center text-2xl text-surface">
+          <code className="text-shadow-none mb-4 inline-block bg-surface p-2 text-xl text-onSurface">
+            NextJS
+          </code>
+          <br className="md:hidden" />
+          <span className="mb-4 inline-block md:mx-4">{` </> `}</span>
+          <br className="md:hidden" />
+          <code className="text-shadow-none mb-4 inline-block bg-surface p-2 text-xl text-onSurface">
+            React
+          </code>
+          <br className="md:hidden" />
+          <span className="mb-4 inline-block md:mx-4">{` </> `}</span>
+          <br className="md:hidden" />
+          <code className="text-shadow-none mb-4 inline-block bg-surface p-2 text-xl text-onSurface">
+            Node.js
+          </code>
+          <br className="md:hidden" />
+          <span className="mb-4 inline-block md:mx-4">{` </> `}</span>
+          <br className="md:hidden" />
+          <code className="text-shadow-none mb-4 inline-block bg-surface p-2 text-xl text-onSurface">
+            TailwindCSS
+          </code>
+          <br className="md:hidden" />
+          <span className="mb-4 inline-block md:mx-4">{` </> `}</span>
+          <br className="md:hidden" />
+          <code className="text-shadow-none bg-surface p-2 text-xl text-onSurface">
+            Git
+          </code>
         </p>
 
-        <div className="flex align-flex justify-center flex-wrap">
-          <a href="https://github.com/NewWorldOrderly" className="m-4 p-6 text-left bg-surface border max-w-sm hover:text-onSurface hover:border-onSurface-alt">
+        <div className="align-flex flex flex-wrap justify-center">
+          <a
+            href="https://github.com/NewWorldOrderly"
+            className="m-4 max-w-sm border bg-surface p-6 text-left hover:border-onSurface-alt hover:text-onSurface"
+          >
             <h2>Github Account &rarr;</h2>
-            <p>Various personal and professional projects I&#39;ve contributed to.</p>
+            <p>
+              Various personal and professional projects I&#39;ve contributed
+              to.
+            </p>
           </a>
 
-          <a href="https://ballersanonymo.us" className="m-4 p-6 text-left bg-surface border max-w-sm hover:text-onSurface hover:border-onSurface-alt">
+          <a
+            href="https://ballersanonymo.us"
+            className="m-4 max-w-sm border bg-surface p-6 text-left hover:border-onSurface-alt hover:text-onSurface"
+          >
             <h2>ballersanonymo.us &rarr;</h2>
             <p>A company focused on full service web development</p>
           </a>
         </div>
       </main>
 
-      <footer className="flex flex-1 py-8 border-t-2 border-onSurface justify-center align-center bg-surface">
+      <footer className="align-center flex flex-1 justify-center border-t-2 border-onSurface bg-surface py-8">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex justify-center align-center grow"
+          className="align-center flex grow justify-center"
         >
-          Powered by{' '}
-          <span className="h-4 ml-2">
+          Powered by{" "}
+          <span className="ml-2 h-4">
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
